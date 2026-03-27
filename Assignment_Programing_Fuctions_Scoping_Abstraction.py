28f083626927ade0a98125676e76f1f23de8e55b
Lelandi DevOps <lelandi.calildeassis@edu.sait.ca>
2026-03-27 12:35:30 -0600

    Cleaning code

--- Assignment_Programing_Fuctions_Scoping_Abstraction.py
+++ Assignment_Programing_Fuctions_Scoping_Abstraction.py
@@ -64,51 +64,22 @@ def book_flight(name, list_flights, bookings, filename):
         
 #View Bookings
 def view_bookings():
-    '''Doc String: This function shows the bookings of a passenger flight and the seats related to the booking'''
-    '''
-    This function will display the current bookings in a user-friendly format.
-    It will iterate through the list of bookings and print out the details of each booking.
-
-    Displays all bookings for the current passenger.  
-• It performs the following steps: 
-• Iterate through each booking in the bookings list. 
-• Process each booking string to extract booking information. 
-• Check if the provided passenger has a booking. 
-• If a booking is found for this passenger: 
-    o  Prints the flight number and seats booked for that booking. 
-    •  If no matching bookings are found, prints “You have no bookings.” 
-• Parameter(s): passenger name, and bookings list. 
-• The function does not return any value including None. 
-
-    '''
-   
+    '''Doc String: This function shows the bookings of a passenger flight and the seats related to the booking'''   
   #  if bookings == "":
     if not bookings:
-        print("No bookings found.")
+        print("You have no bookings.")
         
     for booking in bookings:
         if booking['name'] == passenger_name:
             print(f"Bookings for {passenger_name}")
             print(f"Flight No: {booking['flight number']} Seats Booked: {booking['seats']}")
-  
-
-
-#view_bookings()
 
 #Main Function
 def main():
     '''Doc String: This function calls all the other functions of the reservations'''
-    '''•	It is the main entry point and control function for the flight booking system.  
-OK -    It displays a welcome banner. 
-OK -    It prompts the user for a flight data filename (e.g., flights.txt) and checks if the file exists. 
-OK -	If the file is not found, it displays an error message and prompts again. 
-OK -	If the file is found, it loads the flights data from the file and stores the 
-flights data in a list. 
-OK -	It asks for the passenger's name. 
-•	It continuously displays the main menu until the user chooses to exit (option 5). 
-•	It calls corresponding functions based on user selection 
-•	It displays an invalid input message if the user selects an invalid option 
-'''
+    '''It continuously displays the main menu until the user chooses to exit (option 5). 
+    •	It calls corresponding functions based on user selection 
+    •	It displays an invalid input message if the user selects an invalid option '''
 print("-" * 50)
 print(f"Flight Booking System")
 print("-" * 50)
@@ -139,5 +110,3 @@ passenger_name = input("Enter the passenger name: ")
 print(list_flights)
 book_flight(passenger_name, list_flights, bookings, flightdata)
 view_bookings()
-
-#vincent test push 1
